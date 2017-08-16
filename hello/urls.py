a54f0dbebb103387eaf967959954dfcce1050d85
Kritsana <kritsana.chai@ku.th>
2017-08-17 05:15:35 +0700

    config hello app

---
+++ hello/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+import django.contrib.auth.views
+
+urlpatterns = [
+    url(r'', views.hello),
+]
--- hello/views.py
+++ hello/views.py
@@ -4,3 +4,5 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def hello(request):
+    return render(request,'hello.html',{})
