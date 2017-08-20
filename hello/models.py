7433abe344faeaa01ca9e0c6d73d1c923d88d246
Kritsana <kritsana.chai@ku.th>
2017-08-20 16:31:09 +0700

    add person model

--- hello/models.py
+++ hello/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    GENDER_CHOICES = (
+        ('M', 'male'),
+        ('F', 'Female'),
+    )
+
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
