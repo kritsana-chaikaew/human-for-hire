e8b293e691fe938c3e0afa83b996b78ef7bccd74
Kritsana <kritsana.chai@ku.th>
2017-09-25 20:11:16 +0700

    change db to sqlit3

--- humanforhire/settings.py
+++ humanforhire/settings.py
@@ -134,6 +134,6 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#import dj_database_url
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
