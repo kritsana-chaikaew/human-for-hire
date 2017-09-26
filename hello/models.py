52e328864a22913d386f899eede2a120ac6089e0
olan <olantoj@hotmail.com>
2017-09-26 21:08:53 +0700

    fix conflict

--- hello/models.py
+++ hello/models.py
@@ -1,20 +1,3 @@
 from __future__ import unicode_literals
-<<<<<<< HEAD
-=======
 
 from django.db import models
-import datetime
-
-class Product(models.Model):
-    product_no = models.IntegerField(primary_key=True)
-    seller_username = models.CharField(max_length=20)
-    product_name = models.CharField(max_length=250)
-    product_type = models.CharField(max_length=250)
-    product_details = models.CharField(max_length=2000)
-    date = models.DateField()
-    price = models.IntegerField()
-    tag = models.CharField(max_length=250)
-
-    def __str__(self):
-        return str(self.product_no) + "_" + self.product_name
->>>>>>> 67aa83401e7116fe394454779470147ef6e44926
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
-from .models import Product
+from post.models import Product
 
 class IndexView(generic.ListView):
     template_name = 'index.html'
