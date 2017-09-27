fa5a0df1b462805f1ea6af361a8d35ae5443f85e
kaisukez <zykigix@hotmail.com>
2017-09-28 00:43:04 +0700

    add navbar and can detect wheter user login or not

--- signupLogin/urls.py
+++ signupLogin/urls.py
@@ -4,8 +4,8 @@ import django.contrib.auth.views
 
 app_name = 'signupLogin'
 urlpatterns = [
-    url(r'^signup/$', views.signup),
-    url(r'^login$', views.login),
-    url(r'^main$', views.main),
+    url(r'^signup/$', views.signup, name="signup"),
+    url(r'^login$', views.login, name="login"),
+    url(r'^main$', views.main, name="main"),
     url(r'^logout$', views.logout, name="logout"),
 ]
