916cad584c0d2c4159918190d49b91f195e3cbc8
Kritsana <kritsana.chai@ku.th>
2017-09-28 21:55:39 +0700

    product detail view

--- hello/urls.py
+++ hello/urls.py
@@ -6,4 +6,7 @@ import django.contrib.auth.views
 app_name = 'hello'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^product/(?P<pk>[0-9]+)/$', 
+        views.ProductDetailView.as_view(), 
+        name='product-detail'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 from post.models import Product
+from django.views.generic import DetailView
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'hello/index.html'
@@ -8,3 +10,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Product.objects.order_by('-start_date')
+
+class ProductDetailView(generic.DetailView):
+    template_name = 'hello/product_detail.html'
+    context_object_name = 'product'
+    queryset = Product.objects.all()
+
+    def get_object(self):
+        object = super(ProductDetailView, self).get_object()
+        
+        object.last_accessed = timezone.now()
+        object.save()
+
+        return object
