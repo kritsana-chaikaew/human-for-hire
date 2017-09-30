d57a1ded85129ad1c511c6032beb1410802e6d01
olan <olantoj@hotmail.com>
2017-09-30 15:34:14 +0700

    delete product _type and add init_date

--- post/models.py
+++ post/models.py
@@ -11,13 +11,13 @@ class Product(models.Model):
     seller_username = models.CharField(max_length=250, default='seller_username')
     product_image = models.ImageField(upload_to='img')
     product_name = models.CharField(max_length=250, default='product name')
-    product_type = models.CharField(max_length=250, default='product type')
     start_date = models.DateTimeField(blank=True)
     end_date = models.DateTimeField(blank=True)
     product_details = models.CharField(max_length=2000, default='product details')
     location = models.CharField(max_length=250, default='location')
     price = models.IntegerField(default=0)
     tag = models.CharField(max_length=250, default='tag')
+    init_date = models.DateField(default=datetime.date.today())
 
     def __str__(self):
         return str(self.product_no) + "_" + self.product_name
