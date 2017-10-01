79e177de1a0e9d2f2a4db67d11e0c08f8f37eb09
mameaw14 <pinyapornster@gmail.com>
2017-10-01 15:05:18 +0700

    Edit .gitignore: ignore migrations

--- humanforhire/settings.py
+++ humanforhire/settings.py
@@ -151,8 +151,8 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Update database configuration with $DATABASE_URL.
 #import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
