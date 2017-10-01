ce26f2f1dbeb3eb406c3bcf41b1f8cf4325299d6
mameaw14 <pinyapornster@gmail.com>
2017-10-01 15:19:17 +0700

    uncomment settings.py db

--- humanforhire/settings.py
+++ humanforhire/settings.py
@@ -150,9 +150,9 @@ MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Update database configuration with $DATABASE_URL.
-#import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
