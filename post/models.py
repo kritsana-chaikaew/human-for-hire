4cb1b14f2e5a28e3dd1e1f9c96419002a72dcdc3
kanoonsantikul <niti.sa@ku.th>
2017-10-19 22:34:11 +0700

    modify some part

--- post/models.py
+++ post/models.py
@@ -10,16 +10,15 @@ from taggit.managers import TaggableManager
 # Create your models here.
 class Product(models.Model):
     product_no = models.IntegerField(primary_key=True)
-
     seller_username = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    product_image = models.ImageField(upload_to='img')
     product_name = models.CharField(max_length=250, default='product name')
+    product_image = models.ImageField(upload_to='img')
+    product_details = models.CharField(max_length=2000, default='product details')
+    price = models.IntegerField(default=0)
+    location = models.CharField(max_length=250, default='location')
+    tags = TaggableManager(blank=True)
     start_date = models.DateTimeField(blank=True)
     end_date = models.DateTimeField(blank=True)
-    product_details = models.CharField(max_length=2000, default='product details')
-    location = models.CharField(max_length=250, default='location')
-    price = models.IntegerField(default=0)
-    tags = TaggableManager()
     init_date = models.DateTimeField(default=datetime.datetime.now())
 
     def __str__(self):
--- post/views.py
+++ post/views.py
@@ -7,11 +7,14 @@ from django.core.files.images import ImageFile
 from django.contrib.auth.models import User
 from .models import Product
 
+import taggit
+
 def post(request):
     return render(request,'post.html',{})
 
 def action(request):
     product = Product()
+
     user = User.objects.get(id=request.user.id)
     product.seller_username = user
 
@@ -24,7 +27,22 @@ def action(request):
     product.product_details = request.POST['product_details']
     product.price = request.POST['price']
     product.location = request.POST['location']
-    product.tags = request.POST['tags']
 
+    print(product.pk)
     product.save()
+    print(product.pk)
+
+    # product = Product.objects.create(
+    #     seller_username = User.objects.get(id=request.user.id),
+    #     product_image = ImageFile(request.FILES['product_image']),
+    #     product_name = request.POST['product_name'],
+    #     start_date = request.POST['start_date'],
+    #     end_date = request.POST['end_date'],
+    #     product_details = request.POST['product_details'],
+    #     price = request.POST['price'],
+    #     location = request.POST['location'])
+
+    tag_list = taggit.utils._parse_tags(request.POST['tags'])
+    product.tags.add(*tag_list)
+
     return render(request,'post_respond.html',{})
