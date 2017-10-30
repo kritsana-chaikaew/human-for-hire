c119a2d4165a00fdd0ebccd19e6baab29dcb0ba0
natachit <natachit@hotmail.com>
2017-10-30 19:00:55 +0700

    create report page and add report on navbar

--- report/admin.py
+++ report/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Report
 
-admin.site.register(Report)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'user', 'init_date')
+
+
+admin.site.register(Report, AuthorAdmin)
--- report/models.py
+++ report/models.py
@@ -7,7 +7,7 @@ class Report(models.Model):
     report_no = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     subject = models.CharField(max_length=100, default='report subject')
-    detail = models.CharField(max_length=2000, default='report detail')
+    description = models.CharField(max_length=2000, default='report description')
     init_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
