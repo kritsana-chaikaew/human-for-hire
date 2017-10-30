f7ec39fd43fe9811a1b206ff303a00f945302ea1
kanoonsantikul <niti.sa@ku.th>
2017-10-30 14:11:25 +0700

    change model's field

--- order/models.py
+++ order/models.py
@@ -1,23 +1,22 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
-from post.models import Product
-import datetime
+from django.utils import timezone
 
-# Create your models here.
+from post.models import Product
+
 class Order(models.Model):
     order_no = models.IntegerField(primary_key=True)
     product_no = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     buyer_username = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='buyer')
     seller_username = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='seller')
-    purchase_date = models.DateTimeField(default=datetime.datetime.now())
+    purchase_date = models.DateTimeField(default=timezone.now)
     location = models.CharField(max_length=250, default='location')
     price = models.IntegerField(default=0)
     detail = models.CharField(max_length=250, default='datail')
-    start_date = models.DateTimeField(default=datetime.datetime.now())
-    end_date = models.DateTimeField(default=datetime.datetime.now())
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now)
     STATUS_CODE = (
             (0, 'TO BE ACCEPTED'),
             (1, 'WAITING FOR WORK'),
@@ -30,4 +29,4 @@ class Order(models.Model):
     status = models.PositiveSmallIntegerField(choices=STATUS_CODE, default=0)
 
     def __str__(self):
-        return str(self.order_no) + '_' + str(self.buyer_username) 
+        return str(self.order_no) + '_' + str(self.buyer_username)
--- signupLogin/models.py
+++ signupLogin/models.py
@@ -1,24 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 from django.dispatch import receiver
-import datetime
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     telephone = models.CharField(max_length=200, blank=True)
     address = models.TextField(max_length=500, blank=True)
     gender = models.TextField(max_length=10, blank=True)
-    birthday = models.DateField(default=datetime.date.today)
+    birthday = models.DateField()
     bankaccount = models.CharField(max_length=200, blank=True)
     image = models.ImageField(upload_to='userImage', blank=True)
 
     def __str__(self):
         return str(self.user)
-
-# @receiver(post_save, sender=User)
-# def update_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#     instance.profile.save()
