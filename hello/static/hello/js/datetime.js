a2641df693fd7c225447949687a54cf50567821a
kanoonsantikul <niti.sa@ku.th>
2017-10-31 16:00:52 +0700

    use datetimepicker in every date input

--- hello/static/hello/js/datetime.js
+++ hello/static/hello/js/datetime.js
@@ -1,7 +1,11 @@
-$(".form-date").datetimepicker({
+$(".form-datetime").datetimepicker({
     format: "yyyy-mm-dd hh:ii",
     autoclose: true,
     todayBtn: true,
     minuteStep: 10,
     fontAwesome: true
+});
+
+$('.datepicker').datepicker({
+    format: "yyyy-mm-dd"
 });
