f0edebe7ebc2c04f544d7c764ea7c5af937f405e
mameaw14 <pinyapornster@gmail.com>
2017-11-16 21:31:31 +0700

    Add star_rating.js

---
+++ order/static/order/js/star_rating.js
@@ -0,0 +1,21 @@
+var $star_rating = $('.star-rating .star-icon');
+
+var SetRatingStar = function() {
+    return $star_rating.each(function() {
+        if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+            return $(this).addClass('full');
+        } else {
+            return $(this).removeClass('full');
+        }
+    });
+};
+
+$star_rating.on('click', function() {
+    $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+    return SetRatingStar();
+});
+
+SetRatingStar();
+$(document).ready(function() {
+
+});
